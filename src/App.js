564b881ffb017e9a8e595386072e09bcee313fe5
Erez Shtayman <erezstmn@gmail.com>
2018-02-19 13:41:13 +0200

    get movies button

--- src/App.js
+++ src/App.js
@@ -1,18 +1,35 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
 class App extends Component {
+  constructor(){
+    super();
+    this.handleClick=this.handleClick.bind(this);
+  };
+  handleClick(){
+    axios.post('https://ancient-island-40287.herokuapp.com/movies', {
+      movie_name: 'test',
+      genre: 'test',
+      release_year: 1990
+    })
+    .then(function (response) {
+      console.log(response);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+  }
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Welcome to My Movie App</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <button className='button' onClick={this.handleClick}>Get All movies</button>
+
       </div>
     );
   }
